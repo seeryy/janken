f5cf9557738e5787f1846e68e73bbd4f31a98d1e
seeryy <macuser@net40-dhcp61.sfc.keio.ac.jp>
2017-05-11 11:23:52 +0900

    変更

--- docs/player.js
+++ docs/player.js
@@ -5,11 +5,11 @@ const PA = 2;
 let matches = 0;
 function action(oppornent){
   matches = matches + 1;
-  const remainder = matches % 2;
+  const remainder = matches % 3;
   const isEven = remainder == 0;
   console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
   if(isEven){
-  return GU;
+  return CHOKI;
 }
-return PA;
+return GU;
 }
